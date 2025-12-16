ed16d9a2229434e937e4f672587adfc8a27fa511
andrea201252 <35144222+andrea201252@users.noreply.github.com>
2025-12-16 12:07:07 +0100

    init

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Player } from '../App'
 import './Leaderboard.css'
 
--- src/pages/LevelIntroScreen.tsx
+++ src/pages/LevelIntroScreen.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './LevelIntroScreen.css'
 
 interface LevelIntroScreenProps {
